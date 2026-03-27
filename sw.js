d1e15323a973f10a14c869e35141587ef6189d66
brettfell <70178483+brettfell@users.noreply.github.com>
2026-03-26 23:12:43 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Just a pass-through to satisfy PWA requirements
+});
